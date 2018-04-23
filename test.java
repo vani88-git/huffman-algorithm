5385d7b414301757d90be0f70e9a272982927a47
John Hodson <32934002+hodsonus@users.noreply.github.com>
2018-04-23 11:06:55 -0400

    indent test file

--- test.java
+++ test.java
@@ -4,19 +4,19 @@ public class test {
 	
 	public static void main(String[] args) {
 
-	generateCharFiles.generate("chars.txt", 10000);
+		generateCharFiles.generate("chars.txt", 10000);
 
-	huffman test = new huffman();
+		huffman test = new huffman();
 
-	String inputFile = "/Users/johnhodson/GitHub/huffman-algorithm/illiad.txt";
-	HuffTree tree = test.buildTree(test.buildHeap(test.getFrequencies(new File(inputFile))));
+		String inputFile = "/Users/johnhodson/GitHub/huffman-algorithm/illiad.txt";
+		HuffTree tree = test.buildTree(test.buildHeap(test.getFrequencies(new File(inputFile))));
 
-	System.out.println(test.traverseHuffmanTree(tree));
-	String encoded = test.encodeFile(new File(inputFile), tree);
-	System.out.println(encoded);
+		System.out.println(test.traverseHuffmanTree(tree));
+		String encoded = test.encodeFile(new File(inputFile), tree);
+		System.out.println(encoded);
 
-	String transformed = test.decodeFile( encoded, tree );
+		String transformed = test.decodeFile( encoded, tree );
 
-	System.out.println(transformed);
+		System.out.println(transformed);
 	}
 }
